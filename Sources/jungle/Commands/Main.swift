e43a889e94b571d947ba921d3f63ebb1bd490cb9
Juantri Jimenez <juan.jimenez@xing.com>
2022-11-11 13:25:52 +0000

    New version

--- Sources/jungle/Commands/Main.swift
+++ Sources/jungle/Commands/Main.swift
@@ -5,7 +5,7 @@ struct Jungle: AsyncParsableCommand {
     static var configuration = CommandConfiguration(
         commandName: "jungle",
         abstract: "SwiftPM and Cocoapods based projects complexity analyzer.",
-        version: "2.0.0",
+        version: "2.1.0",
         subcommands: [HistoryCommand.self, CompareCommand.self, GraphCommand.self, ModulesCommand.self],
         defaultSubcommand: CompareCommand.self
     )
