2914b0aae460931127bb135f017a8e762e170983
Juantri94 <55553750+Juantri94@users.noreply.github.com>
2022-11-11 13:29:35 +0000

    Create dependencies module count command (#15)
    * Added modules stats command
    
    * Use modules for SPM
    
    * Control empty nodes
    Added tests
    
    * Refactor some code
    
    * New version

--- Sources/jungle/Commands/Main.swift
+++ Sources/jungle/Commands/Main.swift
@@ -5,8 +5,8 @@ struct Jungle: AsyncParsableCommand {
     static var configuration = CommandConfiguration(
         commandName: "jungle",
         abstract: "SwiftPM and Cocoapods based projects complexity analyzer.",
-        version: "2.0.0",
-        subcommands: [HistoryCommand.self, CompareCommand.self, GraphCommand.self],
+        version: "2.1.0",
+        subcommands: [HistoryCommand.self, CompareCommand.self, GraphCommand.self, ModulesCommand.self],
         defaultSubcommand: CompareCommand.self
     )
 }
---
+++ Sources/jungle/Commands/ModulesCommand.swift
@@ -0,0 +1,73 @@
+import Foundation
+import ArgumentParser
+import Shell
+import PodExtractor
+import DependencyGraph
+import SPMExtractor
+import DependencyModule
+
+struct ModulesCommand: ParsableCommand {
+
+    static var configuration = CommandConfiguration(
+        commandName: "modules",
+        abstract: "Outputs a sorted list of modules dependencies count of your project"
+    )
+
+    @Option(help: "The target in your Podfile or Package.swift file to be used")
+    var target: String
+
+    @Argument(help: "Path to the directory where Podfile.lock or Package.swift is located")
+    var directoryPath: String = "."
+
+    func run() throws {
+        let directoryPath = (directoryPath as NSString).expandingTildeInPath
+        let directoryURL = URL(fileURLWithPath: directoryPath, isDirectory: true)
+    
+        // Check when this contains a Package.swift or a Podfile
+        if FileManager.default.fileExists(atPath: directoryURL.appendingPathComponent("Package.swift").path) {
+            try processPackage(at: directoryURL)
+        } else {
+            try processPodfile(at: directoryURL)
+        }
+    }
+
+    private func processPackage(at directoryURL: URL) throws {
+        let packageRaw = try shell("swift package describe --type json", at: directoryURL)
+        let (allModules, realTargetDependencies) = try extracPackageModules(from: packageRaw, target: target)
+
+        processOutput(realTargetDependencies: realTargetDependencies, allModules: allModules)
+    }
+
+    private func processPodfile(at directoryURL: URL) throws {
+        // Choose the target to analyze
+        let podfileJSON = try shell("pod ipc podfile-json Podfile --silent", at: directoryURL)
+        
+        guard let currentTargetDependencies = try moduleFromJSONPodfile(podfileJSON, onTarget: target) else {
+            throw CompareError.targetNotFound(target: target)
+        }
+        let targetDependencies = currentTargetDependencies.dependencies
+        
+        let podfileLock = try shell("git show HEAD:Podfile.lock", at: directoryURL)
+        let allPodfileModules = try extractModulesFromPodfileLock(podfileLock)
+        let realTargetDependencies = allPodfileModules.filter { targetDependencies.contains($0.name) }
+
+        processOutput(realTargetDependencies: realTargetDependencies.map { $0.name }, allModules: allPodfileModules)
+    }
+    
+    private func processOutput(realTargetDependencies: [String], allModules: [Module]) {
+        let modulesStats: [ModuleStats] = realTargetDependencies
+            .compactMap {
+                guard let graph = try? Graph.makeForModule(name: $0, dependencies: allModules) else { return nil }
+                return ModuleStats(name: $0, numberOfDependencies: graph.nodes.count - 1)
+            }
+            .sorted { $0.numberOfDependencies > $1.numberOfDependencies }
+
+        let result = modulesStats
+            .map { "\($0.numberOfDependencies) - \($0.name)" }
+            .joined(separator: "\n")
+
+        print(
+            result
+        )
+    }
+}
