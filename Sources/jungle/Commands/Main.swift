fc408340635bbdb5486c70d0bd4ebf8b7547f7b8
Oswaldo Rubio <oswaldo.rubio@xing.com>
2022-12-16 13:34:57 +0100

    increase command version

--- Sources/jungle/Commands/Main.swift
+++ Sources/jungle/Commands/Main.swift
@@ -5,7 +5,7 @@ struct Jungle: AsyncParsableCommand {
     static var configuration = CommandConfiguration(
         commandName: "jungle",
         abstract: "SwiftPM and Cocoapods based projects complexity analyzer.",
-        version: "2.1.0",
+        version: "2.1.1",
         subcommands: [HistoryCommand.self, CompareCommand.self, GraphCommand.self, ModulesCommand.self],
         defaultSubcommand: CompareCommand.self
     )
